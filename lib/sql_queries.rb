348e4d6a8868cc7d9fa0d886b941d0956be52075
Kat Maldon <katharinamaldon@gmail.com>
2019-05-16 16:33:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,11 +3,11 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT age FROM bears "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
