4bcf606d6cbf33c14cb6b3c1a559b2b0b18fed15
Kat Maldon <katharinamaldon@gmail.com>
2019-05-16 16:36:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT temperament "
 end
 
 def selects_bear_that_killed_Tim
